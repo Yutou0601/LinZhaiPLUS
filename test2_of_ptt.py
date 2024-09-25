c3a69501d9f3e1f2f3102f58e7f1c38b23e3f48b
Jiaen <94798964+JiaenSuen@users.noreply.github.com>
2024-09-25 22:18:43 +0800

    Update test2_of_ptt.py

--- test2_of_ptt.py
+++ test2_of_ptt.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+import csv
 
 web = 'https://www.ptt.cc/bbs/Soft_Job/index.html'
 content = requests.get(web)            #以get的方式取得網頁
@@ -9,3 +9,14 @@ soup = BeautifulSoup(content.text, "html.parser")
 soup_list = soup.findAll('div', class_="title")
 
 print(soup_list)
+
+
+
+with open('ptt_soft_job_titles.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['標題'])  
+    
+
+    for item in soup_list:
+        title = item.text.strip()  
+        writer.writerow([title])
